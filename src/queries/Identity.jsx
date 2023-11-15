fd4fbc3699b20e3982b05de795f5ff5fa23f776b
AntoineLbrn <antoinelmh@hotmail.fr>
2023-11-15 18:16:54 +0100

    identity e13ified

--- src/queries/Identity.jsx
+++ src/queries/Identity.jsx
@@ -4,6 +4,7 @@ import Query from "../Query";
 const example = [
   ["resource", "http://data-iremus.huma-num.fr/id/18654b25-a05d-415e-b809-e334827edea8"],
   ["getLinkedResourcesIdentity", true],
+  ["appendE13ifiedIdentity", true],
   ["countLinkedResources", true],
   ["linkingPredicate", undefined],
   ["linkedResourcesDirection", "OUTGOING"]
